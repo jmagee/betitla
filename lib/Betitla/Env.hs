ccccc5cd69da26d0cb62a141f19ee57733eb673d
Joshua Magee <magee.joshua@yahoo.com>
2022-11-06 21:45:50 -0800

    Add getEnvRCFrom that takes the RC file path as an argument.

--- lib/Betitla/Env.hs
+++ lib/Betitla/Env.hs
@@ -1,6 +1,7 @@
 module Betitla.Env
 ( Env
 , getEnvRC
+, getEnvRCFrom
 ) where
 
 import           Betitla.Util
@@ -14,6 +15,10 @@ type Env = Map String String
 -- | Get Env from the RC file
 getEnvRC :: IO Env
 getEnvRC = mkHomePath ".betitla.rc" >>= parseAbsFile >>= readEnvRC
+
+-- | Like getEnvRC, but use the specified filepath to find the rc
+getEnvRCFrom :: Path Abs File -> IO Env
+getEnvRCFrom = readEnvRC
 
 readEnvRC :: Path Abs File -> IO Env
 readEnvRC file = unlessEmpty file empty $ \contents ->
